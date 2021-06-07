1e416644d22d3f041a17110168c1b872ad817882
sonnguyengiang <sonstarx2212@gmail.com>
2021-06-07 15:05:39 +0700

    up

---
+++ th2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 20.5f;
+        double c = 20.5;
+        boolean d = true;
+        char e = 'a';
+        String j = "ha noi";
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("j = " + j);
+    }
+}
